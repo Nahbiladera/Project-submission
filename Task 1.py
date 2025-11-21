9453763387552c7a1da861ad0ac99fc231defa67
Nahbiladera <nahbilanjinjoh07@gmail.com>
2025-11-21 19:45:40 +0100

    Initial Commit

---
+++ Task 1.py
@@ -0,0 +1,28 @@
+Guests=['Mr Larry', 'Mrs Fobuzie', 'Miss Dera', 'Mme Gisele', 'Mr Shegu']
+print(Guests)
+print(Guests[0], "You are invited to dinner at my place 7pm")
+print(Guests[1], "You are invited to dinner at my place 7pm")
+print(Guests[2], "You are invited to dinner at my place 7pm")
+print(Guests[3], "You are invited to dinner at my place 7pm")
+print(Guests[4], "You are invited to dinner at my place 7pm")
+
+Guests[2]='Miss Larissa'
+print(Guests)
+print(Guests[0], "You are invited to dinner at my place 7pm")
+print(Guests[1], "You are invited to dinner at my place 7pm")
+print(Guests[2], "You are invited to dinner at my place 7pm")
+print(Guests[3], "You are invited to dinner at my place 7pm")
+print(Guests[4], "You are invited to dinner at my place 7pm")
+
+Guests.append('Mr Breen')
+Guests.insert((0), 'Miss Reena')
+Guests.insert((3),'Mme mademoiselle')
+
+print(Guests)
+print(Guests[0], "You are invited to dinner at my place 7pm")
+print(Guests[1], "You are invited to dinner at my place 7pm")
+print(Guests[2], "You are invited to dinner at my place 7pm")
+print(Guests[3], "You are invited to dinner at my place 7pm")
+print(Guests[4], "You are invited to dinner at my place 7pm")
+print(Guests[5], "You are invited to dinner at my place 7pm")
+print(Guests[6], "You are invited to dinner at my place 7pm")
---
+++ Task 2.py
@@ -0,0 +1,14 @@
+Countries=['Italy', 'France', 'Canada', 'England', 'Great Britain', 'Bali', 'Cali']
+print(Countries)
+Countries.sort()
+print(Countries)
+Countries.reverse()
+print(Countries)
+Countries.reverse()
+print(Countries)
+Countries.sort()
+print(Countries)
+Countries.reverse()
+print(Countries)
+
+
